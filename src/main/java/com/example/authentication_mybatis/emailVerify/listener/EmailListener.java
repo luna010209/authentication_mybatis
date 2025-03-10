10ba22550e10a88ad9796e99e8622fd926840261
Lunado <liendhhha140217@gmail.com>
2025-03-10 21:22:18 +0900

    listener event

--- src/main/java/com/example/authentication_mybatis/emailVerify/listener/EmailListener.java
+++ src/main/java/com/example/authentication_mybatis/emailVerify/listener/EmailListener.java
@@ -1,4 +1,20 @@
 package com.example.authentication_mybatis.emailVerify.listener;
 
-public class EmailListener {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+@Setter
+public class EmailListener extends ApplicationEvent {
+    private String email;
+    private Integer code;
+
+    public EmailListener(String email, Integer code) {
+        super(email);
+        this.email = email;
+        this.code = code;
+    }
 }
---
+++ src/main/java/com/example/authentication_mybatis/emailVerify/listener/EmailListenerEvent.java
@@ -0,0 +1,42 @@
+package com.example.authentication_mybatis.emailVerify.listener;
+
+import com.example.authentication_mybatis.exception.CustomException;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.var;
+import org.springframework.context.ApplicationListener;
+import org.springframework.http.HttpStatus;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
+
+@Component
+@RequiredArgsConstructor
+public class EmailListenerEvent implements ApplicationListener<EmailListener> {
+    private final JavaMailSender javaMailSender;
+    @Override
+    public void onApplicationEvent(EmailListener event) {
+        try{
+            String subject = "Email verification for my website";
+            String senderName = "Luna Do";
+            String content =
+                    "<p>Thank you for your registration with us.<br>" +
+                    "This is a verified code</p>"+
+                    "<b>" + event.getCode()+"</b>" +
+                    "<p>Thank you so much ^^<br><br>"+
+                    "Your sincerely, <br> Luna Do";
+            MimeMessage message = javaMailSender.createMimeMessage();
+            var messageHelper = new MimeMessageHelper(message);
+            messageHelper.setFrom("liendhhha140217@gmail.com", senderName);
+            messageHelper.setTo(event.getEmail());
+            messageHelper.setSubject(subject);
+            messageHelper.setText(content, true);
+            javaMailSender.send(message);
+        } catch (MessagingException | UnsupportedEncodingException e){
+            throw new CustomException(HttpStatus.INTERNAL_SERVER_ERROR, "");
+        }
+    }
+}
